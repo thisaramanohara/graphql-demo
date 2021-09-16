a366ebe054f19e8ee89d816b1d66cd0ee3d0e943
thisaramanohara <thisaramanohara@gmail.com>
2021-09-17 01:48:46 +0530

    Created simple server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Thisara');
+const { ApolloServer, gql } = require('apollo-server')
+const PORT = 4000
+
+//schema
+const typeDefs = gql `
+    type Query {
+        hello: String
+    }
+`
+
+//resolver
+const resolver = {
+    Query: {
+        hello: function() {return 'Hello wecome to GraphQL'}
+    }
+}
+
+const server = new ApolloServer({typeDefs,resolver})
+
+server.listen(PORT).then(({url})=>console.log(`Server started at ${url}`))
+
+
+
